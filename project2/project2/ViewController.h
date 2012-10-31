d8b39d0d6079da648568693085b1e1d004d59186
Rueben Anderson <rueand713@fullsail.edu>
2012-10-31 10:53:01 -0500

    Initial Week 2 commit
    Created my UI for the calculator. Hooked up my IBOutlets their buttons.
    Created my calculators members.

--- project2/project2/ViewController.h
+++ project2/project2/ViewController.h
@@ -9,5 +9,16 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    IBOutlet UITextField *answerField;
+    IBOutlet UISwitch *isSwitchOn;
+    
+    BOOL firstField;
+    NSInteger operation;
+    NSString *answer;
+    NSString *operandA;
+    NSString *operandB;
+}
 
+-(IBAction)onClick:(id)sender;
 @end
--- project2/project2/ViewController.m
+++ project2/project2/ViewController.m
@@ -8,6 +8,14 @@
 
 #import "ViewController.h"
 
+typedef enum {
+    ADD = 0,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE,
+    CLEAR
+} operations;
+
 @interface ViewController ()
 
 @end
@@ -16,6 +24,11 @@
 
 - (void)viewDidLoad
 {
+    // set defaults
+    answer = @"";
+    operandA = @"";
+    operandB = @"";
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -26,4 +39,90 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(IBAction)onClick:(id)sender
+{
+    UIButton *numpad = (UIButton *)sender;
+    if (numpad != nil && isSwitchOn)
+    {
+        NSString *thisValue = [[NSString alloc] initWithFormat:@"%d", numpad.tag];
+        
+        switch (numpad.tag) {
+            case 0:
+                if (firstField)
+                {
+                    // concatenates the string value of the current button to first operand
+                    [operandA stringByAppendingString:thisValue];
+                }
+                else
+                {
+                    // concatenates the string value of the current button to the second operand
+                    [operandB stringByAppendingString:thisValue];
+                }
+                break;
+                
+            case 1:
+                break;
+                
+            case 2:
+                break;
+            
+            case 3:
+                
+                break;
+                
+            case 4:
+                break;
+                
+            case 5:
+                break;
+                
+            case 6:
+                
+                break;
+                
+            case 7:
+                break;
+                
+            case 8:
+                break;
+                
+            case 9:
+                break;
+                
+            case 10:
+                break;
+                
+            case 11:
+                break;
+                
+            case 12:
+                break;
+                
+            case 13:
+                break;
+                
+            case 14:
+                break;
+                
+            default:
+                break;
+        }
+    }
+    else
+    {
+        UISwitch *numpad = (UISwitch *)sender;
+        if (numpad != nil)
+        {
+            
+        }
+        else
+        {
+            UISegmentedControl *numpad = (UISegmentedControl *)sender;
+            if (numpad != nil)
+            {
+                
+            }
+        }
+    }
+}
 @end
